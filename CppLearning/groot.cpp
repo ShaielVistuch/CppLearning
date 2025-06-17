7c17c10c6d2dedd036a230c429f33608a5e591f8
shaiel <shaiel>
2025-06-17 11:00:07 +0300

    fixed according to conventions

--- CppLearning/groot.cpp
+++ CppLearning/groot.cpp
@@ -1,22 +1,15 @@
-// CppLearning.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
+int main() {
     float number = 0;
     std::cout << "Enter a natural number\n";
     std::cin >> number;
-    if (number < 0)
-    {
+    if (number < 0) {
         std::cout << "Number entered is not a natural number! Exiting...\n";
         return 1;
-    }
-    else
-    {
-        std::cout << "The square root of " << number << " is " << std::sqrt(number);
+    } else {
+        std::cout << "The square root of " << number << " is "
+                  << std::sqrt(number);
         return 0;
     }
-
 }
